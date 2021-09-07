1ff7227a33732e3178e2ce13c04ba6ed309ab37c
christian <steveminecraft899@gmail.com>
2021-09-07 16:58:12 -0500

    initial commit

---
+++ app/forms.py
@@ -0,0 +1,59 @@
+from sqlite3 import IntegrityError
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Optional
+from app.models import Post, User
+from flask_login import current_user
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password_confirmation = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("That username is taken. Please choose a different one.")
+    
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError("That email is taken. Please choose a different one.")
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Log In")
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        usernamedata = username.data
+        user = User.query.filter_by(username=username.data).first()
+        if user and usernamedata != current_user.username:
+            raise ValidationError("That username is taken. Please choose a different one.")
+    
+    def validate_email(self, email):
+        emaildata = email.data
+        email = User.query.filter_by(email=email.data).first()
+        if email and emaildata != current_user.email:
+            raise ValidationError("That email is taken. Please choose a different one.")
+
+class MakePost(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(), Length(min=5, max=100)])
+    date_posted = DateField("Date Posted", validators=[Optional()])
+    content = TextAreaField("Content", validators=[DataRequired(), Length(min=1, max=1000)])
+    submit = SubmitField("Make Post")
+
+    def validate_title(self, title):
+        title = Post.query.filter_by(title=title.data).first()
+        if title:
+            raise ValidationError("That title has already been used. Please use another one.")
---
+++ app/routes.py
@@ -0,0 +1,116 @@
+import secrets
+import os
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request
+from app import app, db, bcrypt
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm, MakePost
+from app.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+
+posts = [
+    {
+        'id': 1,
+        'author': 'gamer man',
+        'title': 'blog 1',
+        'content': 'first post woot',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'id': 2,
+        'author': 'book man',
+        'title': 'blog 2',
+        'content': 'second post woot',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"You account has been created! You are now able to log in.", "success")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect (next_page) if next_page else redirect(url_for('home'))
+        flash("Login Unsuccessful. Check email and password.", "danger")
+    return render_template("login.html", title="Login", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+@app.route("/account", methods=["GET", "POST"])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated.", "success")
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data= current_user.email
+    image_file = url_for('static', filename=f'profile_pics/{current_user.image_file}')
+    return render_template("account.html", title="Account", image_file=image_file, form=form)
+
+@app.route("/posts/new", methods=["GET", "POST"])
+@login_required
+def make_post():
+    form = MakePost()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, date_posted=form.date_posted.data or None, content=form.content.data, user_id=current_user.get_id())
+        db.session.add(post)
+        db.session.commit()
+        flash("Post made successfully!", "success")
+        return redirect(f"/posts/{post.id}")
+    return render_template("newpost.html", title="New Post", form=form)
+
+@app.route(f"/posts/<num>")
+def post(num):
+    for post in Post.query.all():
+        if post.id == int(num):
+            return render_template("post.html", title=post.title, post=post)
+    return "<h1>Post not found.</h1>"
